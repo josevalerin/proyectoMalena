117cc725db9aabf3b3a1ec51845f17c1b7c9da9a
Flody27 <diegovv1300@gmail.com>
2022-11-22 12:36:06 -0600

    Agregar archivos de proyecto.

---
+++ TiendaVideojuegos/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaVideojuegos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Consolas()
+        {
+            return View();
+        }
+
+        public ActionResult Accesorios()
+        {
+            return View();
+        }
+
+        public ActionResult Videojuegos()
+        {
+            return View();
+        }
+    }
+}
---
+++ TiendaVideojuegos/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaVideojuegos.Controllers
+{
+    public class UsuarioController : Controller
+    {
+      
+        public ActionResult InicioSesion()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
