c6f36b1728770dfae954367b7250fb0e9d93c0b7
Flody27 <diegovv1300@gmail.com>
2022-11-26 17:19:54 -0600

    UI Crud - Render en tienda

--- TiendaVideojuegos/Controllers/AdminController.cs
+++ TiendaVideojuegos/Controllers/AdminController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
+using System.Net;
 using System.Web.Mvc;
 using TiendaVideojuegos.Models;
+
 namespace TiendaVideojuegos.Controllers
 {
     public class AdminController : Controller
--- TiendaVideojuegos/Controllers/HomeController.cs
+++ TiendaVideojuegos/Controllers/HomeController.cs
@@ -3,29 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TiendaVideojuegos.Models;
+using System.Data.Entity;
+using System.Net;
 
 namespace TiendaVideojuegos.Controllers
 {
     public class HomeController : Controller
     {
+        private TiendaVideojuegosEntities db = new TiendaVideojuegosEntities();
+
         public ActionResult Index()
         {
-            return View();
+            var productos = db.PRODUCTOS.Include(p => p.CATEGORIAS).Include(p => p.MARCAS);
+            return View(productos.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PRODUCTOS productos = db.PRODUCTOS.Find(id);
+            if (productos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productos);
         }
 
         public ActionResult Consolas()
         {
-            return View();
+            var consolas = db.PRODUCTOS.Where(x => x.ID_CATEGORIA == 2);
+            return View(consolas.ToList());
         }
 
         public ActionResult Accesorios()
         {
-            return View();
+            var accesorios = db.PRODUCTOS.Where(x => x.ID_CATEGORIA == 3);
+            return View(accesorios.ToList());
         }
 
         public ActionResult Videojuegos()
         {
-            return View();
+            var videojuegos = db.PRODUCTOS.Where(x => x.ID_CATEGORIA == 1);
+            return View(videojuegos);
         }
     }
 }
