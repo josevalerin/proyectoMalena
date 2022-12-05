5f0e5473979784055948fbd5f888b5419c4694b9
Flody27 <diegovv1300@gmail.com>
2022-12-04 18:24:51 -0600

    Ventas

---
+++ TiendaVideojuegos/Controllers/VENTASController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TiendaVideojuegos.Models;
+
+namespace TiendaVideojuegos.Controllers
+{
+    public class VENTASController : Controller
+    {
+        private TiendaVideojuegosEntities db = new TiendaVideojuegosEntities();
+
+        // GET: VENTAS
+        public ActionResult Index()
+        {
+            var vENTA = db.VENTA.Include(v => v.PRODUCTOS).Include(v => v.USUARIOS);
+            return View(vENTA.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
